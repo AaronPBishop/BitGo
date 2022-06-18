44b809492779e835b2cc3f423352358dfc481c71
AaronPBishop <aaronpbishop@gmail.com>
2022-06-18 14:53:54 -0500

    Created functional timer and restyled interface

--- BitGo.js
+++ BitGo.js
@@ -1,25 +1,33 @@
-let buttons = document.querySelector(".buttons")
+let currentMinute = document.getElementById("current-minute");
+let currentSecond = document.getElementById("current-second");
 
+let buttons = document.querySelector(".buttons");
+
+// Row 1
 let r1c1 = document.getElementById("row1-col1");
 let r1c2 = document.getElementById("row1-col2");
 let r1c3 = document.getElementById("row1-col3");
 let r1c4 = document.getElementById("row1-col4");
 
+// Row 2
 let r2c1 = document.getElementById("row2-col1");
 let r2c2 = document.getElementById("row2-col2");
 let r2c3 = document.getElementById("row2-col3");
 let r2c4 = document.getElementById("row2-col4");
 
+// Row 3
 let r3c1 = document.getElementById("row3-col1");
 let r3c2 = document.getElementById("row3-col2");
 let r3c3 = document.getElementById("row3-col3");
 let r3c4 = document.getElementById("row3-col4");
 
+// Row 4
 let r4c1 = document.getElementById("row4-col1");
 let r4c2 = document.getElementById("row4-col2");
 let r4c3 = document.getElementById("row4-col3");
 let r4c4 = document.getElementById("row4-col4");
 
+// Button matrix
 let matrix = [
     [r1c1, r1c2, r1c3, r1c4],
     [r2c1, r2c2, r2c3, r2c4],
@@ -27,14 +35,14 @@ let matrix = [
     [r4c1, r4c2, r4c3, r4c4]
 ];
 
-const singleClick = () => {
-    buttons.style.backgroundColor = 'red';
+let seconds = 0;
+const timer = (val) => {
+     return val > 9 ? val : "0" + val; 
 };
 
-const doubleClick = () => {
-    buttons.style.backgroundColor= 'blue';
-}
+setInterval(() => {
+    currentMinute.innerHTML = (timer(parseInt(seconds/60,10)));
+    currentSecond.innerHTML = (timer(++seconds%60));
+}, 1000);
 
-document.querySelector(".buttons").addEventListener("click", singleClick);
-
-document.querySelector(".buttons").addEventListener("dblClick", doubleClick);
+timer();
